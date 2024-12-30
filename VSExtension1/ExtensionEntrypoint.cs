6429ce2b377012491d1e7696d83314f624db0c89
Kaz Nishimura <kazssym@linuxfront.com>
2024-12-30 17:27:15 +0900

    Add project files.

---
+++ VSExtension1/ExtensionEntrypoint.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.Extensibility;
+
+namespace VSExtension1
+{
+    /// <summary>
+    /// Extension entrypoint for the VisualStudio.Extensibility extension.
+    /// </summary>
+    [VisualStudioContribution]
+    internal class ExtensionEntrypoint : Extension
+    {
+        /// <inheritdoc/>
+        public override ExtensionConfiguration ExtensionConfiguration => new()
+        {
+            Metadata = new(
+                    id: "VSExtension1.682fdf2e-183c-439e-8304-457aec4697b9",
+                    version: this.ExtensionAssemblyVersion,
+                    publisherName: "Publisher name",
+                    displayName: "VSExtension1",
+                    description: "Extension description"),
+        };
+
+        /// <inheritdoc />
+        protected override void InitializeServices(IServiceCollection serviceCollection)
+        {
+            base.InitializeServices(serviceCollection);
+
+            // You can configure dependency injection here by adding services to the serviceCollection.
+        }
+    }
+}
