545afb2e98b5c918df9b992c6afe3d12941ca1e1
Kaz Nishimura <kazssym@linuxfront.com>
2025-01-03 18:52:33 +0900

    Add global function `command1` to `__init__.js`
    Introduced a new global function `command1` in the `__init__.js` file.
    This function outputs "command1 called" using `extension.Output.WriteLine`
    when invoked, enhancing the script with a new executable command.

--- VSExtension1/scripts/__init__.js
+++ VSExtension1/scripts/__init__.js
@@ -3,3 +3,7 @@
 // This file will be used to initialize the ScriptEngine.
 
 extension.Output.WriteLine("Hello from __init__.js")
+
+globalThis.command1 = function () {
+    extension.Output.WriteLine("command1 called")
+}
